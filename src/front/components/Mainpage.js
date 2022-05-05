5cb29c88fb6fc8f133098320637aa69968055e93
omarmt29 <omarmendezt29@gmail.com>
2022-05-05 10:51:16 -0400

    maquetando

---
+++ src/front/components/Mainpage.js
@@ -0,0 +1,56 @@
+
+import logo from '../source/fondo2.png';
+import mainpage from '../style/mainpage.css';
+
+export default function Mainpage(){
+
+    return(
+        <>
+            <div className="Main-container">
+
+                {/* LOGO */}
+                <div className='logo-container'>
+                    <img src={logo}
+                        alt='logo' id='logo' />
+
+                </div>
+
+                {/* NAVBAR */}
+                <div className='navbar-container '>
+                    <li class="nav-list" id="menu">
+                        <a>Inicio</a>
+                        <a>Sobre nosotros</a>
+                        <a>Actividadess</a>
+                        <a>Transparencia</a>
+                        <a>Contáctanos</a>
+                    </li>
+
+                </div>
+
+
+
+                
+
+                {/* SLIDER A LA IZQUIERDA */}
+
+                {/* ASIDE DE NOTICIAS LA DERECHA */}
+
+                {/* DIRECTIVA SLIDER */}
+
+                {/* FOOTER */}
+
+
+
+
+
+
+
+
+
+            </div>
+
+
+        </>
+    )
+
+}
